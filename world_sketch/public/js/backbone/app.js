c5794d5c5fefa5f66d704bdf0f2310b769c29c1e
Kristen Tonga <kristen.tonga@gmail.com>
2014-07-16 12:46:03 -0400

    ready to deploy to heroku

--- world_sketch/public/js/backbone/app.js
+++ world_sketch/public/js/backbone/app.js
@@ -43,14 +43,6 @@ Sketcher.initialize = function(){
 
     debugger;
   });
-
-  bab = new Sketcher.Models.Sketch({data: '<div class=​"etch-a-sketch">​<svg><line x1=​"102.90277862548828" y1=​"80.376708984375" x2=​"102.5" y2=​"79" style=​"stroke-width:​ 40px;​ stroke:​ yellow;​">​</line>​<circle cx=​"102.5" cy=​"79" r=​"20" style=​"fill:​ rgb(255, 255, 0)​;​">​</circle>​<line x1=​"102.5" y1=​"79" x2=​"104" y2=​"76.5" style=​"stroke-width:​ 40px;​ stroke:​ yellow;​">​</line>​<circle cx=​"104" cy=​"76.5" r=​"20" style=​"fill:​ rgb(255, 255, 0)​;​">​</circle>​</svg></div>',
-                                    artist:'Bob',
-                                    location: 'NY',
-                                  })
-  babView = new Sketcher.Views.SketchView({model: bab});
-  sketches.add(bab)
-  sketches.add({artist: 'Miss'})
 };
 
 $(function(){
